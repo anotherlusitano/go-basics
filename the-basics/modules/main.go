47a989a101bcd3223fcbd01d923af34dd9cdf285
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2026-07-26 17:41:59 +0100

    Pointers, defer & panic

--- the-basics/modules/main.go
+++ the-basics/modules/main.go
@@ -6,14 +6,6 @@ import (
 
 var packageVariable = "I am a package variable\n"
 
-func init() {
-	fmt.Println("A")
-}
-
-func init() {
-	fmt.Println("B")
-}
-
 func calculateTax(price float32) (float32, float32) {
 	return price * 0.09, price * 0.02
 }
@@ -24,12 +16,37 @@ func calculateTaxWithNames(price float32) (stateTax float32, cityTax float32) {
 	return
 }
 
-func main() {
-	stateTax, cityTax := calculateTax(100)
-	stateTax2, _ := calculateTaxWithNames(100)
+func birthday(age int) {
+	age = age + 1
+}
 
-	fmt.Println(stateTax, cityTax)
-	fmt.Println(stateTax2)
+func birthdayReal(age *int) {
+	defer fmt.Println("Happy birthday!")
+
+	if *age > 120 {
+		// panic is just to exit the program, but it is not a good practice to use panic for error handling
+		panic("You shall not live that long!")
+	}
+
+	fmt.Printf("Pointer: %v \nValue: %v \n", age, *age)
+
+	*age++
+}
+
+func main() {
+	// stateTax, _ := calculateTaxWithNames(100)
+	// fmt.Println(stateTax)
+
+	// will execute at the end of the function, even if there is a panic
+	defer fmt.Println("Byeee")
+	defer fmt.Println("The first to enter is the last to leave, like a stack!")
+
+	age := 30
+	birthday(age)
+	fmt.Println(age)
+
+	birthdayReal(&age)
+	fmt.Println(age)
 
 	printSomeData()
 }
