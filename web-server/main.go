8aa6468ea079fd484334b8947e6e87fe33b2a3d7
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2026-08-10 16:38:24 +0100

    web: add client-side site

--- web-server/main.go
+++ web-server/main.go
@@ -7,12 +7,18 @@ import (
 
 const addr string = ":3333"
 
-func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello from a Go program"))
-	})
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from a Go program"))
+}
 
-	err := http.ListenAndServe(addr, nil)
+func main() {
+	server := http.NewServeMux()
+	server.HandleFunc("/hello", handleHello)
+
+	fs := http.FileServer(http.Dir("./public"))
+	server.Handle("/", fs)
+
+	err := http.ListenAndServe(addr, server)
 	if err != nil {
 		fmt.Println("Error: couldn't open the server")
 	}
