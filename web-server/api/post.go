7d69854943e3d335e9973f7e0dd3b93aa40c6881
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2026-08-17 17:23:41 +0100

    web: add POST method to the API

---
+++ web-server/api/post.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"server.web/data"
+)
+
+func Post(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Unsupported Method", http.StatusMethodNotAllowed)
+	}
+
+	var exhibition data.Exhibition
+
+	err := json.NewDecoder(r.Body).Decode(&exhibition)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	data.Add(exhibition)
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Ok"))
+}
